f3ca713bb86ecdfd7e7dc23569ee8b6c0dd29220
jerskine07 <73801121+jerskine07@users.noreply.github.com>
2021-01-02 19:11:32 +0000

    1

--- README.txt
+++
@@ -1,5 +0,0 @@
-git init
-git add README.txt
-git commit -m "first commit"
-git remote add origin git@github.com:{jerskine07}/{ELE2024CWGroup14}.git
-git push origin master
--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+git init
+git add README.txt
+git commit -m "first commit"
+git remote add origin git@github.com:{jerskine07}/{ELE2024CWGroup14}.git
+git push origin master
