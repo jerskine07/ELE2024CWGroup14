4547c931805a14d7c14a99dc8dc9edc73e529252
jerskine07 <73801121+jerskine07@users.noreply.github.com>
2021-01-11 17:40:40 +0000

    1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-git init
-git add README.txt
-git commit -m "first commit"
-git remote add origin git@github.com:{jerskine07}/{ELE2024CWGroup14}.git
-git push origin master
+#########################################################
+# Problem B2
+#########################################################
