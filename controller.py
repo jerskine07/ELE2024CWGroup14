9fa4a85ab7afb235d53031a48b33f10c36d1f914
jerskine07 <73801121+jerskine07@users.noreply.github.com>
2021-01-11 22:46:33 +0000

    1

---
+++ controller.py
@@ -0,0 +1,11 @@
+class Controller:
+
+    def __init__(self, time_constant=30, sensor):
+
+
+
+
+    def control(self, y, set_point=0):
+
+
+        return x
--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 import numpy as np
 import sympy as sm
 import matplotlib.pyplot as plt
-import math
 
 m = 0.425
 g = 9.81
@@ -29,12 +28,17 @@ xmin = d + (m * g * np.sin(theta) / (k))
 xmax = delta
 print(xmin)
 print(xmax)
-xe = np.array((range(100)))
 # At equilibrium no acceleration or velocity: x_2=0 x_3=0
 x2=0
+num_points = 500
+xe = np.linspace(xmin, xmax, num_points)
 y=alpha-xe
 L=L0+L1*np.exp(-alpha*y)
-Ve = np.array
-Ve =np.array(np.sqrt((7*m*(R*y+L)^2)*((5*k(xe-d)/(7*m))-(5*g*np.sin(theta)/7))))
+V = np.sqrt((7*m*(R*y+L)**2)*((5*k*(xe-d)/(7*m))-(5*g*np.sin(theta)/7)))
 
-plt.plot(xe,Ve)
+plt.plot(xe, V)
+plt.xlabel('xe')
+plt.ylabel('V')
+plt.title('Plot to show V between Xmin and Xmax')
+plt.grid()
+plt.show()
